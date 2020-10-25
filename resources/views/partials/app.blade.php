685ec1a012aba599a76b0fadcc48c8264e173f5d
Alonso Alas <alonsoalas44@gmail.com>
2020-10-25 15:24:19 -0600

    Creacion del navbar, footer y una vista previa del home

---
+++ resources/views/partials/app.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <link rel="stylesheet" href="{{asset('css/welcome.css')}}">
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    {{-- Agregando FontAwesome mediante CDN   --}}
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+
+    @include('layout.nav')
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+
+    @include('layout.footer')
+
+    </div>
+</body>
+</html>
---
+++ resources/views/partials/news.blade.php
@@ -0,0 +1,142 @@
+<div class="container">
+    <div class="row">
+        <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+            <h3 class="font-weight-bold mt-3 mb-3">Politicas Publicas</h3>
+        </div>
+        <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+            <h3 class="font-weight-bold mt-3 mb-3">Busqueda por:</h3>
+        </div>
+
+
+        <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+
+            <table class="table table-bordered">
+                <tr>
+                    <th width="60" class="bg-principal"></th>
+                    <th>
+                        <p class="font-weight-bold">Politicas publicas en Educacion</p>
+                        <small>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis doloribus optio quod
+                            sapiente voluptas. Assumenda doloremque facere molestiae odit rem.
+                        </small>
+                    </th>
+                </tr>
+                <tr>
+                    <th class="bg-principal"></th>
+                    <th>
+                        <p class="font-weight-bold">Politicas publicas sobre Agua</p>
+                        <small>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid assumenda enim id iure
+                            maxime minima minus nihil repudiandae? Beatae, cum!
+                        </small>
+                    </th>
+                </tr>
+                <tr>
+                    <th class="bg-principal"></th>
+                    <th>
+                        <p class="font-weight-bold">Politicas publicas Territoriales</p>
+                        <small>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad deserunt dolorem, illo
+                        </small>
+                    </th>
+                </tr>
+                <tr>
+                    <th class="bg-principal"></th>
+                    <th>
+                        <p class="font-weight-bold">Politicas publicas Migratorias</p>
+                        <small>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad deserunt dolorem, illo
+                        </small>
+                    </th>
+                </tr>
+            </table>
+
+        </div>
+
+        <div class="col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
+            <table class="table table-bordered">
+                <th class="bg-principal">
+                    Nombre
+                </th>
+                <tr>
+                    <td>Jorge P. <span class="badge badge-pill badge-secondary ml-5">OO</span></td>
+                </tr>
+                <tr>
+                    <td>Jorge P. <span class="badge badge-pill badge-secondary ml-5">OO</span></td>
+                </tr>
+                <tr>
+                    <td>Jorge P. <span class="badge badge-pill badge-secondary ml-5">OO</span></td>
+                </tr>
+                <tr>
+                    <td>Jorge P. <span class="badge badge-pill badge-secondary ml-5">OO</span></td>
+                </tr>
+                <tr>
+                    <td>Jorge P. <span class="badge badge-pill badge-secondary ml-5">OO</span></td>
+                </tr>
+                <tr>
+                    <td>Jorge P. <span class="badge badge-pill badge-secondary ml-5">OO</span></td>
+                </tr>
+                <tr>
+                    <td>Jorge P. <span class="badge badge-pill badge-secondary ml-5">OO</span></td>
+                </tr>
+            </table>
+        </div>
+        <div class="col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
+            <table class="table table-bordered">
+                <th class="bg-principal">
+                    AUTOR
+                </th>
+                <tr>
+                    <td>Teoria <span class="badge badge-pill badge-secondary ml-5">OO</span></td>
+                </tr>
+                <tr>
+                    <td>Teoria <span class="badge badge-pill badge-secondary ml-5">OO</span></td>
+                </tr>
+                <tr>
+                    <td>Teoria <span class="badge badge-pill badge-secondary ml-5">OO</span></td>
+                </tr>
+                <tr>
+                    <td>Teoria <span class="badge badge-pill badge-secondary ml-5">OO</span></td>
+                </tr>
+                <tr>
+                    <td>Teoria <span class="badge badge-pill badge-secondary ml-5">OO</span></td>
+                </tr>
+                <tr>
+                    <td>Teoria <span class="badge badge-pill badge-secondary ml-5">OO</span></td>
+                </tr>
+                <tr>
+                    <td>Teoria <span class="badge badge-pill badge-secondary ml-5">OO</span></td>
+                </tr>
+            </table>
+        </div>
+        <div class="col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
+            <table class="table table-bordered">
+                <th class="bg-principal">
+                    FECHA
+                </th>
+                <tr>
+                    <td>2010 - 2020 <span class="badge badge-pill badge-secondary ml-3">OO</span></td>
+                </tr>
+                <tr>
+                    <td>2010 - 2020 <span class="badge badge-pill badge-secondary ml-3">OO</span></td>
+                </tr>
+                <tr>
+                    <td>2010 - 2020 <span class="badge badge-pill badge-secondary ml-3">OO</span></td>
+                </tr>
+                <tr>
+                    <td>2010 - 2020 <span class="badge badge-pill badge-secondary ml-3">OO</span></td>
+                </tr>
+                <tr>
+                    <td>2010 - 2020 <span class="badge badge-pill badge-secondary ml-3">OO</span></td>
+                </tr>
+                <tr>
+                    <td>2010 - 2020 <span class="badge badge-pill badge-secondary ml-3">OO</span></td>
+                </tr>
+                <tr>
+                    <td>2010 - 2020 <span class="badge badge-pill badge-secondary ml-3">OO</span></td>
+                </tr>
+            </table>
+        </div>
+
+    </div>
+</div>
